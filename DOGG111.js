552e887f3ef7a679415171ece79da5302671df9b
VarvaraYakusheva <varvaraakuseva904@gmail.com>
2026-05-25 14:19:48 +0500

    Add files via upload

---
+++ DOGG111.js
@@ -0,0 +1,24 @@
+const IMAGE_SRCS = [
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/TAX.png",
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/BULDAK.png",
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/OVCHAR.png",
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/DOLMA.png",
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/PEKIN.png",
+  "https://raw.githubusercontent.com/VarvaraYakusheva/Piski/refs/heads/main/RETVIL.png"
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "ТАКСА",
+
+  "ФРАНЦУЗКИЙ БУЛЬДОГ",
+
+  "ОВЧАРКА",
+
+  "ДАЛМАТИНЕЦ",
+
+  "ПЕКИНЕС",
+
+  "ЗОЛОТИСТЫЙ РЕТРИВЕР"
+
+];
